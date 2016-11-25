df2cc2710073490d5f85e4239041471d0a6399ca
Hatsune Miku <psqmaxor@gmail.com>
2016-11-25 12:26:45 +0100

    guide translation

--- web/src/pages/Guide.jsx
+++ web/src/pages/Guide.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import {Alert, Col, Grid, Row} from 'react-bootstrap'
-import {translate} from 'react-i18next'
+import {Interpolate, translate} from 'react-i18next'
 
 var Guide = React.createClass({
 
@@ -16,109 +16,54 @@ var Guide = React.createClass({
 				<Grid>
 					<Row>
 						<Col md={12}>
-							<h1>The Guide</h1>
+							<h1>{t('guide:title')}</h1>
 							<Alert bsStyle="danger">
-								<strong>I swear to God.</strong> No matter how simple to follow the guide is, you people <b>still</b> manage to get completely lost.<br />
-								If you can't understand what is happening, read it again. And again. Ask a friend to help you understand it. Call your mom. Cry a lot.<br />
-								Make sure there's absolutely nothing more you can do before you start asking for help.
+								<Interpolate i18nKey='guide:alert' useDangerouslySetInnerHTML={true} />
 							</Alert>
 
-							<h2>Table of Contents</h2>
+							<h2>{t('guide:toc.title')}</h2>
 							<br />
-							<a href="#guide-what">What's Mikuia?</a>
+							<a href="#guide-what">{t('guide:toc.questions.what')}</a>
 							<br />
-							<a href="#guide-how">How to enable Mikuia?</a>
+							<a href="#guide-how">{t('guide:toc.questions.how')}</a>
 							<br />
-							<a href="#guide-commands">How to add commands?</a>
+							<a href="#guide-commands">{t('guide:toc.questions.commands')}</a>
 							<br />
-							<a href="#guide-games">Where can I find game specific features?</a>
+							<a href="#guide-games">{t('guide:toc.questions.games')}</a>
 							<br />
-							<a href="#guide-no">Why is Mikuia not working?</a>
+							<a href="#guide-no">{t('guide:toc.questions.no')}</a>
 							<br />
-
 
 							<hr />
 
-
 							<div id="guide-what">
-								<h1>What's Mikuia?</h1>
-								<p>Mikuia is a Twitch bot.</p>
+								<h1>{t('guide:toc.questions.what')}</h1>
+								<p><Interpolate i18nKey='guide:toc.answers.what' useDangerouslySetInnerHTML={true} /></p>
 							</div>
 
 							<div id="guide-how">
-								<h1>How to enable Mikuia?</h1>
-								<p>
-									<ol>
-										<li>
-											Login using the "Login with Twitch" button in the top right corner.
-										</li>
-										<li>
-											Click "Dashboard" in the top right corner.
-										</li>
-										<li>
-											Click "Settings".
-										</li>
-										<li>
-											Click "Enable".
-										</li>
-									</ol>
-								</p>
+								<h1>{t('guide:toc.questions.how')}</h1>
+								<p><Interpolate i18nKey='guide:toc.answers.how' useDangerouslySetInnerHTML={true} /></p>
 							</div>
 
 							<div id="guide-commands">
-								<h1>How to add commands?</h1>
-								<p>
-									<ol>
-										<li>
-											Open the "Dashboard".
-										</li>
-										<li>
-											Click "Commands".
-										</li>
-										<li>
-											Click "Add a command".
-										</li>
-										<li>
-											Follow the pop-up.
-										</li>
-									</ol>
-								</p>
+								<h1>{t('guide:toc.questions.commands')}</h1>
+								<p><Interpolate i18nKey='guide:toc.answers.commands' useDangerouslySetInnerHTML={true} /></p>
 							</div>
 
 							<div id="guide-games">
-								<h1>Where can I find game specific features?</h1>
-								<p>
-									<ol>
-										<li>
-											Open the "Dashboard".
-										</li>
-										<li>
-											Click "Plugins".
-										</li>
-										<li>
-											Find a game you care about and click "Enable".
-										</li>
-										<li>
-											Click "Settings" next to the button you just clicked.
-										</li>
-									</ol>
-								</p>
+								<h1>{t('guide:toc.questions.games')}</h1>
+								<p><Interpolate i18nKey='guide:toc.answers.games' useDangerouslySetInnerHTML={true} /></p>
 							</div>
 
 							<div id="guide-no">
-								<h1>Why is Mikuia not working?</h1>
-								<p>
-									99% of the time, it's your fault. Triple check the settings.
-									<br />
-									Mikuia joins your channel after a short period of time from you going live.
-								</p>
+								<h1>{t('guide:toc.questions.no')}</h1>
+								<p><Interpolate i18nKey='guide:toc.answers.no' useDangerouslySetInnerHTML={true} /></p>
 							</div>
 
 							<hr />
 
-							This guide is short because I honestly don't give a fuck.
-							<br />
-							If you need more help, try asking on <a href="http://discord.mikuia.tv">Discord</a>, or use the live chat (violet bubble in bottom right corner).
+							<p><Interpolate i18nKey='guide:whatever' useDangerouslySetInnerHTML={true} /></p>
 
 						</Col>
 					</Row>
