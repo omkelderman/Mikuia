a7270e8bf67df767a2689cce4edd31784ca006f4
Laura Lynn <hatsu@hatsu.tv>
2017-04-15 14:28:52 +0200

    bork

--- ts/lib/services/twitchService.ts
+++ ts/lib/services/twitchService.ts
@@ -15,8 +15,10 @@ import {Tools} from '../tools';
 import {TwitchGetLiveStreamsResponse} from '../responses/twitchGetLiveStreamsResponse';
 
 export class TwitchService implements MikuiaService {
-	private channelsJoined = [];
+	private channelsJoined: Array<string> = [];
+	private connectionChannels = {};
 	private connections = {};
+	private idMappings = {};
 
 	private joinLimiter = limiter({
 		interval: 10 * 1000,
@@ -40,33 +42,51 @@ export class TwitchService implements MikuiaService {
 	}
 
 	async join(channel: Channel) {
-		if(channel.type == 'twitch') {
-			if(!(channel.id in this.channelsJoined)) {
-				var limitEntries = await this.db.zrangebyscoreAsync('service:twitch:limiter:join', '-inf', '+inf');
-				var currentTime = (new Date).getTime() * 1000;
-				var remainingRequests = 49;
+		return new Promise(async (resolve) => {
+			if(channel.type == 'twitch') {
+				if(!(channel.id in this.channelsJoined)) {
+					/*	Uhhh, I think this deserves an explanation.
+						I don't have one.
+						This has been working for like a year or so, it's probably fine. */
 
-				for(let limitEntry of limitEntries) {
-					if(parseInt(limitEntry) + 15 * 1000 * 1000 > currentTime) {
-						remainingRequests--;
-					}
-				}
+					var limitEntries = await this.db.zrangebyscoreAsync('service:twitch:limiter:join', '-inf', '+inf');
+					var currentTime = (new Date).getTime() * 1000;
+					var remainingRequests = 49;
 
-				if(remainingRequests > 0) {
-					this.joinLimiter('', (err, timeLeft) => {
-						if(!timeLeft) {
-							this.connections[0].join(channel.name).catch((err) => {
-								Log.error('Twitch', 'Failed to join channel: #' + channel.name + '.');
-								console.log(err);
-							})
+					for(let limitEntry of limitEntries) {
+						if(parseInt(limitEntry) + 15 * 1000 * 1000 > currentTime) {
+							remainingRequests--;
 						}
-					})
+					}
+
+					if(remainingRequests > 0) {
+						this.joinLimiter('', (err, timeLeft) => {
+							if(!timeLeft) {
+								this.connections[0].join(channel.name).then(() => {
+									resolve();
+								}).catch((err) => {
+									Log.error('Twitch', 'Failed to join channel: #' + channel.name + '.');
+									console.log(err);
+									resolve(err);
+								})
+							} else {
+								resolve(true);
+							}
+						})
+					} else {
+						resolve(true);
+					}
+				} else {
+					resolve(true);
 				}
+			} else {
+				resolve(true);
 			}
-		}
+		});
 	}
 
 	spawnConnection(id: number) {
+		var self = this;
 		return new Promise((resolve) => {
 			var logHeader = '[' + cli.cyanBright(id) + ']';
 
@@ -87,6 +107,8 @@ export class TwitchService implements MikuiaService {
 			client.id = id;
 			client.connect();
 
+			this.connectionChannels[client.id] = [];
+
 			client.on('connected', (address: string, port: number) => {
 				Log.info('Twitch', logHeader + ' Connected to ' + cli.yellowBright(address + ':' + port) + '.');
 				resolve(client);
@@ -95,23 +117,31 @@ export class TwitchService implements MikuiaService {
 			client.on('disconnected', (reason: string) => {
 				Log.error('Twitch', logHeader + ' Disconnected.');
 				console.log(reason);
+
+				// TODO: clear this.connectionChannels & join the channels back
 			})
 
 			client.on('join', (channel: string, username: string) => {
 				if(username == this.settings.services.twitch.username.toLowerCase()) {
-					Log.info('Twitch', logHeader + ' Joined channel: ' + channel + '.');
+					Log.info('Twitch', logHeader + ' Joined channel: ' + cli.yellowBright(channel) + '.');
+
+					this.channelsJoined.push(channel);
+					this.connectionChannels[client.id].push(channel);
 				}
 			})
 
 			client.on('message', (channel: string, userstate: any, message: string, self: boolean) => {
-				if(message.toLowerCase().indexOf('lukanya') > -1) {
-					Log.info('Twitch', logHeader + ' ' + cli.yellowBright(channel) + ' / ' + cli.greenBright(userstate.username) + ': ' + message);
+				if(message.toLowerCase().indexOf(this.settings.services.twitch.username.toLowerCase()) > -1) {
+					Log.info('Twitch', logHeader + ' ' + cli.yellowBright(channel) + ' ' + cli.yellow('(' + this.idMappings[channel] + ')') + ' / ' + cli.greenBright(userstate.username) + ': ' + message);
 				}
 			})
 
 			client.on('part', (channel, username) => {
 				if(username == this.settings.services.twitch.username.toLowerCase()) {
 					Log.info('Twitch', logHeader + ' Left channel: ' + channel + '.');
+
+					this.channelsJoined.splice(this.channelsJoined.indexOf(channel), 1);
+					this.connectionChannels[client.id].splice(this.connectionChannels[client.id].indexOf(channel), 1);
 				}
 			})
 		})
@@ -125,7 +155,7 @@ export class TwitchService implements MikuiaService {
 	}
 
 	async parseChunk(chunk): Promise<TwitchGetLiveStreamsResponse> {
-		return new Promise<TwitchGetLiveStreamsResponse>((resolve, reject) => {
+		return new Promise<TwitchGetLiveStreamsResponse>((resolve) => {
 			// console.log(cli.magenta(chunk.join(',')));
 			request({
 				url: 'https://api.twitch.tv/kraken/streams/?channel=' + chunk.join(',') + '&client_id=' + this.settings.services.twitch.clientId + '&api_version=5'
@@ -159,7 +189,10 @@ export class TwitchService implements MikuiaService {
 				var data: TwitchGetLiveStreamsResponse = await this.parseChunk(chunk);
 				for(let stream of data.streams) {
 					var channel = this.getChannel(stream.channel.name);
-					this.join(channel);
+					
+					await this.join(channel);
+					
+					this.idMappings['#' + stream.channel.name] = stream.channel._id;
 				}
 			}
 
